c10cd11b8d9869bb6de21e454c92a73b7a1d7e92
jkefeli <127637467+jkefeli@users.noreply.github.com>
2023-06-08 19:38:32 -0400

    Delete M01_DistantMetastasis/CUIMC_Test directory

--- M01_DistantMetastasis/CUIMC_Test/M01_example.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 nohup python3 testbestmodel_CUIMC_M01.py m bigbird 1024 CUIMC_M01/Final_M01.csv model_output/m01_output/m_rs1000_bigbird_4bsize_1024max_tokens_lowerLR09-05-2022_06h-23m/  > M01_CUIMC_Test.txt  2>&1 &
